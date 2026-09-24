3dd4c9d011aba47c0b26ce191a54352cd766ac8d
Anton Lennström <antonlen@kth.se>
2026-09-24 14:56:11 +0200

    add time4io Anton version

--- lab3-files/time4io/labmain.c
+++ lab3-files/time4io/labmain.c
@@ -14,9 +14,81 @@ extern void tick(int*);
 extern void delay(int);
 extern int nextprime( int );
 
-int led_counter = 0;
-int mytime = 0x5957;
+int mytime = 0x005959;
 char textstring[] = "text, more text, and even more text!";
+
+
+int clamp(int value, int min, int max) {
+  if (value < min) return min;
+  if (value > max) return max;
+  return value;
+}
+
+
+void set_leds(int led_mask) {
+  volatile int* led_toggle = (volatile int*) 0x04000000;
+  *led_toggle = led_mask & 0x3FF;
+}
+
+
+void set_displays(int display_number, int value, int dot){
+  int start_address = 0x04000050;
+  int offset = 0x10 * (clamp(display_number, 0, 5));
+
+  volatile int* segment_pointer = (volatile int*) (start_address + offset);
+
+  int display_bits;
+  switch (value){
+    case 0:
+      display_bits = 0b11000000;
+      break;
+    case 1:
+      display_bits = 0b11111001;
+      break;
+    case 2:
+      display_bits = 0b10100100;
+      break;
+    case 3:
+      display_bits = 0b10110000;
+      break;
+    case 4:
+      display_bits = 0b10011001;
+      break;
+    case 5:
+      display_bits = 0b10010010;
+      break;
+    case 6:
+      display_bits = 0b10000010;
+      break;
+    case 7:
+      display_bits = 0b11111000;
+      break;
+    case 8:
+      display_bits = 0b10000000;
+      break;
+    case 9:
+      display_bits = 0b10011000;
+      break;
+    default:
+      display_bits = 0b11000000;
+      break;
+  }
+  if (dot)
+    display_bits = display_bits & 0b01111111;
+
+  *segment_pointer = display_bits; 
+}
+
+ int get_sw(void){
+  volatile int* switch_values = (volatile int*) 0x04000010;  
+  return (*switch_values & 0x3FF);
+ }
+
+ int get_btn(void) {
+  volatile int* button_value = (volatile int*) 0x040000d0;  
+  return (*button_value & 0x1); 
+ }
+
 
 /* Below is the function that will be called when an interrupt is triggered. */
 void handle_interrupt(unsigned cause) 
@@ -26,145 +98,70 @@ void handle_interrupt(unsigned cause)
 void labinit(void)
 {}
 
-
-void set_leds(int led_mask){
-
-  volatile int *p = (volatile int *)0x04000000;
-  *p = led_mask;
-
-}
-
-void increment_led(){
-  set_leds(led_counter);
-  led_counter++;
-}
-
-void set_displays(int display_number, int value){
-
-  volatile int *display_pointer = (volatile int *)(0x04000050 + display_number * 0x10);
-
-  switch(value){
-    case 0:
-      *display_pointer = 0b11000000;
-      break;
-    case 1:
-      *display_pointer = 0b11111001;
-      break;
-
-    case 2:
-      *display_pointer = 0b10100100;
-      break;
-    
-    case 3:
-      *display_pointer = 0b10110000;
-      break;
-
-    case 4:
-      *display_pointer = 0b10011001;
-      break;
-
-    case 5:
-      *display_pointer = 0b10010010;
-      break;
-
-    case 6:
-      *display_pointer = 0b10000010;
-      break;
-
-    case 7:
-      *display_pointer = 0b11111000;
-      break;
-
-    case 8:
-      *display_pointer = 0b10000000;
-      break;
-
-    case 9:
-      *display_pointer = 0b10011000;
-      break;
-
-    default:
-      break;
-  }
-}
-
-int get_sw(void){
-
-  volatile int *sw_pointer = (volatile int *)0x04000010;
-
-  int sw = *sw_pointer & 0b1111111111;
-
-  return sw;
-}
-
-int get_btn(void){
-
-  volatile int *btn_pointer = (volatile int *)0x040000d0;
-
-  int btn = *btn_pointer & 0b1;
-
-  return btn;
-}
-
 /* Your code goes into main as well as any needed functions. */
 int main() {
   // Call labinit()
   labinit();
-  
+
+  int counter = 0;
+  while (counter <= 15) { 
+    set_leds(counter);
+    delay(2);
+    counter++;
+  }
+
   // Enter a forever loop
   while (1) {
-    if(get_btn()){
-      int sw_control = (get_sw() >> 8);
-      int sw_values;
-      sw_values = get_sw();
+    if (get_btn()) {
+      int sw_values = get_sw();
+      int msb2_sw = sw_values  >> 8;
+      int lsb6_sw = (sw_values & 0b111111);
 
-      sw_values &= 0b00111111; 
+      int tens = lsb6_sw / 10;
+      int ones = lsb6_sw % 10;
+      int display_value = (tens << 4) | ones;
 
-      int sw_values_tens = sw_values / 10;
-      int sw_values_ones = sw_values % 10;
+      int reset_sw = (sw_values & 0b1000000);
 
-      int sw_display_values = (sw_values_tens << 4) | sw_values_ones;
-    
-      switch(sw_control){
+      int masked_time;
+      switch (msb2_sw){
         case 1:
-          mytime &= 0xFFFF00;
-          mytime = mytime | sw_display_values;
+          masked_time = mytime & 0xFFFF00;
+          mytime = masked_time | display_value;
           break;
-        
         case 2:
-          sw_display_values = sw_display_values << 8;
-          mytime &= 0xFF00FF;
-          mytime = mytime | sw_display_values;
-          break;
-        
+          masked_time = mytime & 0xFF00FF;
+          mytime = masked_time | (display_value << 8);
+          break; 
         case 3:
-        sw_display_values = sw_display_values << 16;
-          mytime &= 0x00FFFF;
-          mytime = mytime | sw_display_values;
-          break;
-
+          masked_time = mytime & 0x00FFFF;
+          mytime = masked_time | (display_value << 16);
+          break;     
         default:
           break;
       }
+
+      if (reset_sw)
+        mytime = 0x0;
     }
-
-    set_displays(0, (mytime & 0xF));
-    set_displays(1, (mytime & 0xF0) >> 4);
-    set_displays(2, (mytime & 0xF00) >> 8);
-    set_displays(3, (mytime & 0xF000) >> 12);
-
-    if((mytime & 0xF0000) >> 16 > 9){
-      mytime = mytime & 0xF0FFFF;
+    
+    set_displays(0, (mytime & 0x00000F), 0);
+    set_displays(1, (mytime & 0x0000F0) >> 4, 0);
+    set_displays(2, (mytime & 0x000F00) >> 8, 1);
+    set_displays(3, (mytime & 0x00F000) >> 12, 0);
+    
+    if (((mytime & 0x0F0000) >> 16) > 9){
+      mytime &= 0xF0FFFF;
       mytime += 0x100000;
     }
 
-    set_displays(4, (mytime & 0xF0000) >> 16);
+    set_displays(4, (mytime & 0x0F0000) >> 16, 1);
 
-    if((mytime & 0xF00000) >> 20 > 9){
+    if (((mytime & 0xF00000) >> 20) > 9)
       mytime &= 0x0FFFFF;
-    }
+  
+    set_displays(5, (mytime & 0xF00000) >> 20, 0);
 
-    set_displays(5, (mytime & 0xF00000) >> 20);
 
     time2string( textstring, mytime ); // Converts mytime to string
     display_string( textstring ); //Print out the string 'textstring'
@@ -172,3 +169,5 @@ int main() {
     tick( &mytime );     // Ticks the clock once
   }
 }
+
+
